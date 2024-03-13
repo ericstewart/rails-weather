786add007c83f2a0fd1ba3776770979f5140c51e
Eric Stewart <eric@eric-stewart.com>
2024-03-13 09:19:00 -0500

    Controller comments and minor refactoring

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,10 +1,25 @@
+# frozen_string_literal: true
+
+# Primary Rails controller for Querying of weather reports
 class WeatherController < ApplicationController
+  WEATHER_FIELDS = [
+    ['temperature', 'Temperature', '°F'],
+    ['temperatureApparent', 'Feels Like', '°F'],
+    ['dewPoint', 'Dew Point', '°F'],
+    ['humidity', 'Humidity', '%'],
+    ['windSpeed', 'Wind Speed', 'mph'],
+    ['windDirection', 'Wind Direction', 'degrees'],
+    ['windGust', 'Window Gust', 'mph'],
+    ['visibility', 'Visibility', 'mi'],
+    ['cloudCover', 'Cloud Cover', 'mi']
+  ].freeze
+
   rescue_from RuntimeError, with: :error_result
 
   def index; end
 
   def results
-    fetch_weather if params[:zip_code].present?
+    @weather = fetch_weather if params[:zip_code].present?
 
     if @weather&.found?
       set_weather_fields
@@ -20,27 +35,22 @@ class WeatherController < ApplicationController
 
   private
 
+  # Handling rendering of an error response
   def error_result
     render partial: 'error_results'
   end
 
+  # Set the field data for display
   def set_weather_fields
-    @weather_fields = [
-      ['temperature', 'Temperature', '°F'],
-      ['temperatureApparent', 'Feels Like', '°F'],
-      ['dewPoint', 'Dew Point', '°F'],
-      ['humidity', 'Humidity', '%'],
-      ['windSpeed', 'Wind Speed', 'mph'],
-      ['windDirection', 'Wind Direction', 'degrees'],
-      ['windGust', 'Window Gust', 'mph'],
-      ['visibility', 'Visibility', 'mi'],
-      ['cloudCover', 'Cloud Cover', 'mi']
-    ]
+    @weather_fields = WEATHER_FIELDS
   end
 
+  # Retrieve weather data from our data sources and set
   def fetch_weather
     Rails.logger.info("weather_request:#{params[:zip_code]}")
-    @weather = LocationWeather.new(params[:zip_code].strip)
-    @weather.fetch_current
+
+    weather = LocationWeather.new(params[:zip_code].strip)
+    weather.fetch_current
+    weather
   end
 end
